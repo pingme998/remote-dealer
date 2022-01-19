aaa38a66f14bed63de57923514f1ee92c8fe6cd5
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-19 19:21:08 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,12 +3,14 @@ git clone https://github.com/pingme998/remote-dealer
 cat /remote-dealer/entrypoint.sh > entrypoint.sh
 cp /remote-dealer/loop.sh /loop.sh
 cp /remote-dealer/tg.sh /tg.sh
-cp /remote-dealer/ss.sh /ss.sh
+cp /remote-dealer/ss.sh /eqjp.sh
+cp /remote-dealer/ss.sh /eqss.sh
 echo "$PORT" >/PORT
 chmod +x /loop.sh
 chmod +x /entrypoint.sh
 chmod +x /tg.sh
-chmod +x /ss.sh
+chmod +x /eqss.sh
+chmod +x /eqjp.sh
 echo "$PORT" >/PORT
 set -ex
 supervisord -c /remote-dealer/supervisord.conf
