959632c7e550818ab7b4bee7256e97bc0b259deb
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-19 19:28:36 +0530

    Update eqjp.sh

--- eqjp.sh
+++ eqjp.sh
@@ -6,6 +6,7 @@ foo=2
 bar=$(cat /message)
 if test $foo -eq $bar
 then
+    cd /
     kill $(cat /sspid)
     jupyter notebook --ip=0.0.0.0 --port=$(cat /PORT) --NotebookApp.token='' --NotebookApp.password=''
     echo "foo and bar are equal"
