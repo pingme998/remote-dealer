878659823e26cf85b72cfeac78c2b9c4f39ec82f
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-20 11:37:21 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git clone https://github.com/pingme998/remote-dealer
 cat /remote-dealer/entrypoint.sh > entrypoint.sh
-cp /remote-dealer/loop.sh /loop.sh
-cp /remote-dealer/tg.sh /tg.sh
-cp /remote-dealer/eqjp.sh /eqjp.sh
-cp /remote-dealer/eqss.sh /eqss.sh
+cat /remote-dealer/loop.sh >/loop.sh
+cat /remote-dealer/tg.sh >/tg.sh
+cat /remote-dealer/eqjp.sh >/eqjp.sh
+cat /remote-dealer/eqss.sh >/eqss.sh
 echo "$PORT" >/PORT
 chmod +x /loop.sh
 chmod +x /entrypoint.sh
