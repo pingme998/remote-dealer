5c69b1f663ed5a095ef86b7354418f6e183c9504
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-20 11:30:30 +0530

    Update tg.sh

--- tg.sh
+++ tg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :
 do
-sleep 2
 curl "$PINGBOT" |grep ',"text":"' |tail -1 |sed 's/,/\n/g' |grep '"text":"'|sed 's|"text":"||g' |sed 's|"}}]}||g' |grep "1\|2" >/message
+sleep 30
 done
